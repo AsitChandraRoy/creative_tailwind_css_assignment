425a3822c91d47f2fdd741789ca31ba482c6486e
AsitChandraRoy <asitchandraroy@gmail.com>
2024-07-07 21:28:07 +0600

    first commit

--- script.js
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-const htmlTag = document.getElementById("htmlTag");
-const switchToggle = document.getElementById("switch");
-switchToggle.addEventListener("click", function () {
-  htmlTag.classList.toggle("dark");
-});
